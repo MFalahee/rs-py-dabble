fbfea9e9ade696da090a009340ec13904e8d81c0
mjfalahee <mjfalahee@gmail.com>
2023-11-02 13:54:55 -0700

    this works much better

--- scripts/flick.py
+++ scripts/flick.py
@@ -12,8 +12,9 @@ class TickFlick():
     def __init__(self, corner):
         self.mode = 'flick'
         self.corner = corner
-        self.start = time.time()
-        self.region = self.location_scaled_to_window(600, 640, 110, 140)
+        self.start_time = time.time()
+        self.last_click = time.time()
+        self.region = self.location_scaled_to_window(610, 640, 110, 140)
         listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
         listener.start()
   
@@ -28,9 +29,10 @@ class TickFlick():
             key = key.split('.')[1]
         if key == "q":
             self.mode = "flick"
-        else:
+        elif key == "w":
             self.mode = "idle"
-
+        elif key == "esc":
+            self.mode = "done"
     def on_press(self, key):
         if DEBUG: 
             try:
@@ -46,32 +48,36 @@ class TickFlick():
             image_path = os.path.join(image_dir, image_file)
             location = pyautogui.locateOnScreen(image_path, confidence=0.9, region=region)
             if location:
+                print(image_file)
                 return location
         return None
 
     def flick(self):
-        start_time = time.time()
-        max_loops = 1000
+        max_loops = 100
         loops = 0
         pyautogui.click()
-        running_time = start_time
-        last_click = 0
-        while self.mode == "flick":
-            running_time = time.time() - running_time
+        self.last_click = self.start_time
+        time_passed = 0
+        while self.mode != "done":
+            time_passed += (time.time() - self.start_time)
             loops += 1
-            if loops % 5 == 0:
-                print("Loop: " + str(loops))
-                print("Running for: " + str(round(running_time, 2)) + " seconds" + "|||||| + Last click: " + str(round(last_click, 2)) + " seconds ago")
-            cycle = self.search_for_images(self.region)
+            if self.mode == "idle":
+                loops = 0
+                time.sleep(0.05)
             if loops >= max_loops:
                 self.mode = "idle"
                 break
-            if cycle != None:
-                time.sleep((random.random() / 100 ) + 0.02)
+            if loops % 5 == 0:
+                print("Loop: " + str(loops))
+                # print("Running for: " + str(round(self.start_time, 2)) + " seconds" + "|||||| + Last click: " + str(round(self.last_click, 2)) + " seconds ago")
+            toggleSpots = self.search_for_images(self.region)
+            if (toggleSpots != None or self.last_click > 0.7) and self.mode == "flick":
+                print(toggleSpots, self.last_click)
+                time.sleep((random.random() / 100) + 0.03)
                 pyautogui.click()
-                last_click = time.time() - start_time
+                time.sleep((random.random() / 100) + 0.43)
+                pyautogui.click()
+                self.last_click = time.time() - self.last_click
             else:
-                if last_click >= 0.7:
-                    pyautogui.click()
-                    last_click = time.time() - start_time
                 time.sleep(0.05)
+                continue
